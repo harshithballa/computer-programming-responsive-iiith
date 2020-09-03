22fe6873b551b7ecc777c56f9f17dcb87d62ee41
sai harshith balla <saiharshithballa99@gmail.com>
2020-09-03 22:44:52 +0530

    disable manual input

--- src/lab/exp4/simulation/1-D/array.js
+++ src/lab/exp4/simulation/1-D/array.js
@@ -83,8 +83,10 @@ window.view = {
 	},
 	takeInputFromRadioBox: function() {
 		var element = document.getElementsByName('radio_group')
-		if ( element[0].checked )
+		if ( element[0].checked ){
+			document.getElementById("userInput").disabled = true
 			this.generateRandomNumbers()
+		}
 		else if (element[1].checked)
 			this.getUserInput()
 	},
